f337af8c4df7123473ffb9628bef9a25fd34cdc3
Yustis-Kof <kofyustis@yandex.ru>
2026-07-15 23:53:54 +0300

    удалил ненужные файлы

--- local_game.py
+++ local_game.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     биба = Bot(0, 'биба', dictionary=bot_dictionary)
     боба = Player(1, 'боба')
     game = Game(board=None, players=[биба], host=боба, name="Игра", dictionary=game_dictionary)
+    game.start()
     game.skip_move()
     while not game.winners:
         print(", ".join([game.players[i].name + ": " + str(game.count[i]) for i in range(len(game.players))]))
